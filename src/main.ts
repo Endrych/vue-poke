6a125acee57215a2015afe8a692facc02294a0eb
David Endrych <endrych@awebsys.cz>
2023-12-07 13:14:20 +0100

    Install axios

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import store from './store/store'
+import store from "./store/store";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 const app = createApp(App);
 
+app.use(VueAxios, axios);
+app.provide("axios", app.config.globalProperties.axios);
 app.use(router);
 app.use(store);
 

c1935d30d9bb355532eda9fa5e8e71e0d9ef88d4
David Endrych <endrych@awebsys.cz>
2023-12-07 13:35:50 +0100

    Store for pokemons page

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,71 @@
-import { createStore } from "vuex";
+import { createStore, Store } from "vuex";
+import { getIndexFromPokemonUrl } from "../utils";
+import { InjectionKey } from "vue";
+import { State } from "../models/state.model";
+import RestClient from "../api/RestClient";
 
-const store = createStore({
-	state: {},
-	mutations: {},
-	actions: {},
-	getters: {},
+// define injection key
+export const key: InjectionKey<Store<State>> = Symbol();
+
+const client = new RestClient();
+
+const store = createStore<State>({
+	state: {
+		loaded: false,
+		items: [],
+		previousUrl: null,
+		nextUrl: null,
+	},
+	mutations: {
+		setLoaded(state, payload) {
+			state.loaded = payload;
+		},
+		setResultData(state, payload) {
+			state.previousUrl = payload.previous;
+			state.nextUrl = payload.next;
+			state.items = payload.results.map((r: any) => ({
+				name: r.name,
+				url: r.url,
+				index: getIndexFromPokemonUrl(r.url),
+			}));
+			state.loaded = true;
+		},
+	},
+	actions: {
+		async loadItems({ commit }) {
+			commit("setLoaded", false);
+			const result = await client.getPokemons(null);
+			commit("setResultData", result);
+		},
+		async previousItems({ commit, state }) {
+			if (state.previousUrl) {
+				commit("setLoaded", false);
+				const result = await client.getPokemons(state.previousUrl);
+				commit("setResultData", result);
+			}
+		},
+		async nextItems({ commit, state }) {
+			if (state.nextUrl) {
+				commit("setLoaded", false);
+				const result = await client.getPokemons(state.nextUrl);
+				commit("setResultData", result);
+			}
+		},
+	},
+	getters: {
+		loaded(state) {
+			return state.loaded;
+		},
+		pokemons(state) {
+			return state.items;
+		},
+		isFirstPage(state) {
+			return state.previousUrl === null;
+		},
+		isLastPage(state) {
+			return state.nextUrl === null;
+		},
+	},
 });
 
 export default store;

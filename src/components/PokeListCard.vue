4a17e83334c925a3d5e744268a7cadd7ac34679f
David Endrych <endrych@awebsys.cz>
2023-12-07 08:25:54 +0100

    Prepare basic card for poke list

---
+++ src/components/PokeListCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png">
+        <h2>{{ "bulbasaur" }}</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.card {
+    display: inline-block;
+    background: #fff;
+    border-radius: 15px;
+    -webkit-box-shadow: 7px 1px 15px 0px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 7px 1px 15px 0px rgba(0, 0, 0, 0.75);
+    box-shadow: 7px 1px 15px 0px rgba(0, 0, 0, 0.75);
+}
+
+h2 {
+    color: #000;
+    text-transform: capitalize;
+}
+</style>
--- src/pages/PokeList.vue
+++ src/pages/PokeList.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
+        <PostListCard />
         <Pagination @previous="previous" @next="next" :is-first="true" :is-last="false" />
     </div>
 </template>
 
 <script setup lang="ts">
+import PostListCard from '../components/PokeListCard.vue'
 import Pagination from '../components/Pagination.vue'
 
 function previous() {
